80db47bcd5878f0990937477a7fa43c0113c5bec
slatime <slatime@naver.com>
2022-12-13 23:08:16 +0900

    add vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'ipsul', // 사이트 타이틀
+    description: '연습장.',
+    themeConfig: {
+      logo: 'https://avatars1.githubusercontent.com/u/18749057?s=460&v=4', // 로고 이미지
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Home2', link: '/' },
+      ],
+      sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+    }
+  }
